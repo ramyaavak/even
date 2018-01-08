817334a35698bc3475de09c3b2c748f57888e8fa
ramyaavak <35220550+ramyaavak@users.noreply.github.com>
2018-01-08 23:14:37 +1200

    evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x;
+printf("Enter the value of x:");
+scanff("%d",&x);
+if(x%2==0)
+{
+printf("The number is even");
+}
+else
+{
+printf("The number is odd");
+}}
